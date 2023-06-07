fef6aa56adc33fce634308df64403da84392a102
samueldsj1 <samueldsj1@gmail.com>
2023-06-07 17:47:56 -0300

    atualizando

---
+++ Models/Competencia.cs
@@ -0,0 +1,9 @@
+using WebAppBloom.Controllers;
+namespace WebAppBloom.Models;
+
+public class Competencia{
+   
+    public int Id {get ; set ;}
+    public string? ColunaBloom{get; set;}
+    public string? LinhaBloom { get; set; }
+}
